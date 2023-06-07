237972d0b6a18a45a4c73127d45c77872ee22ff8
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-06-07 11:50:06 -0400

    Complete exercise 2

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const cors = require('cors')
 const express = require('express')
+const promisePool = require('./PromisePool.js').promisePool
+const MySqlProxy = require('./mySqlProxy.js')
 const { body, check, param, validationResult } = require('express-validator')
 
 const PORT = 80
@@ -17,6 +19,17 @@ app.use(express.urlencoded({extended: true}))
 app.get('/message', cors(corsOptions), async (req, res) => {
     res.send({message: 'Hello World!!!'})
     })
+
+
+// Get Car
+app.get('/car/:carId', cors(corsOptions), async (req, res) =>{
+    const { carId } = req.params;
+    // const SELECT_CAR = "select * from car where car_id = ?"
+    const results = await promisePool.query("select * from car where car_id = ?", [carId]);
+    console.log(results[0])
+    res.send(results[0])
+})
+   
 
 app.listen(PORT, () => {
     console.log(`Express web API running on port: ${PORT}.`)
